566b776114a5266fc336a5b2cf2d060760674dd5
Matheus Tardivo <matheustardivo@gmail.com>
2011-04-29 12:31:26 -0300

    Added operations to store languages in a file

--- lib/tradutor.rb
+++ lib/tradutor.rb
@@ -6,10 +6,9 @@ class Tradutor
   
   attr_reader :languages
   
-  QUERY_STRING = "/language/translate/v2?key=[google_key]&q=[query]&source=[language_source]&target=[language_target]";
-  LANGUAGES_QUERY_STRING = "/language/translate/v2/languages?key=[google_key]";
-  GOOGLE_APIS_URL = 'https://www.googleapis.com';
+  GOOGLE_APIS_URL = 'https://www.googleapis.com'
   
+  QUERY_STRING = "/language/translate/v2?key=[google_key]&q=[query]&source=[language_source]&target=[language_target]"
   QUERY_KEYS = {
     :key => '[google_key]', 
     :query => '[query]', 
@@ -17,12 +16,14 @@ class Tradutor
     :target => '[language_target]'
   }
   
+  LANGUAGES_QUERY_STRING = "/language/translate/v2/languages?key=[google_key]"
+  LANGUAGE_FILE = 'languages.json'
+  
   def initialize(google_key = GOOGLE_TRANSLATE_KEY)
     QUERY_STRING.sub!(QUERY_KEYS[:key], google_key)
     LANGUAGES_QUERY_STRING.sub!(QUERY_KEYS[:key], google_key)
     prepare_connection
-    
-    @languages = fetch_languages
+    @languages = read_languages
   end
   
   def translate(opts = {})
@@ -40,23 +41,20 @@ class Tradutor
     raise "Invalid target language" unless language_valid?(target)
   end
   
+  # User library can update language file
+  def update_languages_file
+    File.delete(LANGUAGE_FILE) if File.exist?(LANGUAGE_FILE)
+    File.open(LANGUAGE_FILE, 'w') do |file|
+      file.puts fetch_languages
+    end
+  end
+  
   private
   
   def prepare_connection
     url = URI.parse(GOOGLE_APIS_URL)
     @http = Net::HTTP.new(url.host, url.port)
     @http.use_ssl = true if url.scheme == 'https'
-  end
-  
-  def fetch_languages
-    request = Net::HTTP::Get.new(LANGUAGES_QUERY_STRING)
-    json = @http.request(request).body
-    
-    languages = []
-    obj = JSON.parse(json)
-    obj["data"]["languages"].each { |lang| languages << lang["language"] }
-    
-    languages
   end
   
   def prepare_query_string(query, source, target)
@@ -68,6 +66,29 @@ class Tradutor
     query_string.sub!(QUERY_KEYS[:target], target)
     
     query_string
+  end
+  
+  # Language management - refactor it!
+  
+  # Try to read languages from file
+  # If the file not exist, fetch languages from Google and create it
+  def read_languages
+    update_languages_file unless File.exist?(LANGUAGE_FILE)
+    json = File.readlines(LANGUAGE_FILE).join
+    convert_json_languages_to_array(json)
+  end
+  
+  def fetch_languages
+    request = Net::HTTP::Get.new(LANGUAGES_QUERY_STRING)
+    @http.request(request).body
+  end
+  
+  def convert_json_languages_to_array(json)
+    languages = []
+    obj = JSON.parse(json)
+    obj["data"]["languages"].each { |lang| languages << lang["language"] }
+    
+    languages
   end
   
   def language_valid?(lang)
